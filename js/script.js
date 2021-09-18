d629c6058d7dd48bfaed1636f567478e110ce53d
Wojciech Kandziora <wojciech.kandziora@gmail.com>
2021-09-18 20:25:46 +0200

    Remove class 'active' in script.js

--- js/script.js
+++ js/script.js
@@ -9,11 +9,19 @@ document.getElementById('test-button').addEventListener('click', function(){
     console.log('Link was clicked!', event);
   
     /* remove class 'active' from all article links  */
-  
+    const activeLinks = document.querySelectorAll('.titles a.active');
+
+    for(let activeLink of activeLinks){
+      activeLink.classList.remove('active');
+    }
     /* add class 'active' to the clicked link */
   
     /* remove class 'active' from all articles */
-  
+    const activeArticles = document.querySelectorAll('.active');
+
+    for(let activeArticle of activeArticles){
+      activeArticle.classList.remove('active');
+    }
     /* get 'href' attribute from the clicked link */
   
     /* find the correct article using the selector (value of 'href' attribute) */
